02672552738f825516b0fb7bd0b59bf2fbf37533
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-02-17 22:46:11 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+  
 end

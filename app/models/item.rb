659ec0d435e85ba005246af8a139a1c84b44ff9d
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-02-18 00:00:19 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  validates :name, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :sales_status_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
 end

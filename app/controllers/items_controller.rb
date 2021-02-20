2abf5c2b4e5f29ba8a2987e890db80068187e3af
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-02-20 17:41:14 +0900

    商品情報編集機能の実装　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
-  before_action :move_to_edit, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_edit, only: [:edit, :update]
 
   def index
     @items = Item.all.order(id: :DESC)
@@ -20,15 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -41,6 +41,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_edit

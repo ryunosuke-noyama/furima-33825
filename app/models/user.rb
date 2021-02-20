645cd735ac01769e865cce6522883be724910cfb
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-02-20 11:11:11 +0900

    userモデルバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}+\z/i }
-    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }

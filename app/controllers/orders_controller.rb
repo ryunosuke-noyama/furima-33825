d1becd5b260ce54d04c7dec7d7e5efeb80a3e384
ryunosuke-noyama <ryu1998.08.21@icloud.com>
2021-03-06 16:21:01 +0900

    商品購入機能　修正3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create, :move_to_index, :sold_out_item, :pay_item]
   before_action :move_to_index, only: [:index, :create]
   before_action :sold_out_item, only: [:index, :create]
